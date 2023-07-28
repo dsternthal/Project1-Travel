f6f242dd3c2cb412d930397fdbd84161ba09e200
Dani Sternthal <dstrnthl97@gmail.com>
2023-07-27 20:05:48 -0400

    added navbar

--- Assets/script.js
+++ Assets/script.js
@@ -25,4 +25,15 @@ async function initMap(lat,long) {
   });
 }
 
-getOpenWeather("Chicago")
+getOpenWeather("Miami")
+
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
