4c9a577c4d9f79b068354a853248145d56c4d8ea
Dani Sternthal <dstrnthl97@gmail.com>
2023-08-03 22:30:58 -0400

    save

--- Assets/script.js
+++ Assets/script.js
@@ -3,9 +3,10 @@ var cityName = document.querySelector(".uk-search-input")
 var searchButton = document.querySelector(".searchButton")
 var stateEl = document.querySelector("#states")
 var restaurantSectionEl = document.querySelector("#restaurantSection")
-var recentSearch = document.querySelector("#recentSearch")
+var recentSearch = document.querySelector(".recentSearch")
 var topSearchEl = document.getElementById("topSearch")
-var recentCity = []
+var recentCity
+var recentState
 
 
 
@@ -17,7 +18,7 @@ var recentCity = []
 
 function getOpenWeather(cityName, state) {
   var apiKey = "43307f36c133c1b4d80feb3644b2ab3e"
-  var URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + ","+ state +"&appid=" + apiKey
+  var URL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "," + state + "&appid=" + apiKey
   console.log(URL)
   fetch(URL)
     .then(function (response) {
@@ -40,32 +41,16 @@ async function initMap(lat, long) {
   });
 }
 
-//Dani's API
-// const url = 'https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/MI/city/West%20Bloomfield/0';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'ff27872189msh4ec125b3b4946c2p10f900jsnd9403e0ba202',
-// 		'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.json();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
 
 
-//Lina's API version
+
+//Restaurants Near Me API 
 async function searchRestaurantStateCity(city, state) {
   const url = 'https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/' + state + '/city/' + city + '/0';
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '13c7831628msh5863dc064ac246bp12d132jsn9912df302347',
+      'X-RapidAPI-Key': '03b3452f5amsh2d4fc3a4a3f1e7fp1d9c33jsna13422f96a90',
       'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
     }
   };
@@ -78,7 +63,7 @@ async function searchRestaurantStateCity(city, state) {
     console.error(error);
   }
 }
-async function restaurantName(){
+async function restaurantName() {
   var searchRestaurants = await searchRestaurantStateCity(cityName.value, stateEl.value.split(",")[0])
   console.log(searchRestaurants)
   for (let i = 0; i < 6; i++) {
@@ -89,46 +74,48 @@ async function restaurantName(){
     cardEl.textContent = resName
     var resDesc = document.getElementById("d" + i)
     resDesc.textContent = cuisineType
-    resDesc.link= searchRestaurants.restaurants[i].website
-    document.getElementById("L" + i).href= searchRestaurants.restaurants[i].website
+    resDesc.link = searchRestaurants.restaurants[i].website
+    document.getElementById("L" + i).href = searchRestaurants.restaurants[i].website
   }
-var searchedCity = {
-  name: cityName.value,
-  state: stateEl.value.split(",")[0]
-}
-if (recentCity>=3) {
-  recentCity.shift()
+  var searchedCity = cityName.value
+  var searchedState = stateEl.value.split(",")[0]
   recentCity.push(searchedCity)
-}
-else{
-  recentCity.push(searchedCity)
-}
-localStorage.setItem("pastSearch", JSON.stringify(searchedCity))
-getLocal()
+  recentState.push(searchedState)
+
+  localStorage.setItem("pastSearchCity", JSON.stringify(recentCity))
+  localStorage.setItem("pastSearchState", JSON.stringify(recentState))
 }
 
-async function getLocal(){
-var localStorageCity = JSON.parse(localStorage.getItem("pastSearch"))
-console.log(localStorageCity)
-if (!localStorageCity) {
-  recentCity=[]
-  return
-}
-console.log(recentCity)
+async function getLocal() {
+  var localStorageCity = JSON.parse(localStorage.getItem("pastSearchCity"))
+  var localStorageState = JSON.parse(localStorage.getItem("pastSearchState"))
+  console.log(localStorageCity)
+  console.log(localStorageState)
+  if (!localStorageCity) {
+    recentCity = []
+    recentState = []
+    return
+  }
+  else {
+    recentState=localStorageState
+    recentCity=localStorageCity
+    for (let i = 0; i < recentCity.length; i++) {
+      var recentLiEl = document.createElement("li")
+      var recentButtonEl = document.createElement("button")
+      recentButtonEl.textContent=recentCity[i]
+      recentLiEl.appendChild(recentButtonEl)
+      recentSearch.appendChild(recentLiEl)
+    }
+  }
+    console.log(recentCity)
+  }
 
-await recentCity.push(localStorageCity)
-for (let i = 0; i < recentCity.length; i++) {
-  var recentButtonEl = document.getElementById("top"+i)
-  recentButtonEl.textContent=recentCity[i].name
-}
-}
+  getLocal()
 
-getLocal()
-
-searchButton.addEventListener("click", function(event){
-  event.preventDefault()
-getOpenWeather(cityName.value,stateEl.value.split(",")[1]) 
-restaurantName()
-//searchRestaurantStateCity(cityName.value, stateEl.value.split(",")[0])
-restaurantSectionEl.setAttribute("class","")
-})
+  searchButton.addEventListener("click", function (event) {
+    event.preventDefault()
+    getOpenWeather(cityName.value, stateEl.value.split(",")[1])
+    restaurantName()
+    //searchRestaurantStateCity(cityName.value, stateEl.value.split(",")[0])
+    restaurantSectionEl.setAttribute("class", "")
+  })
